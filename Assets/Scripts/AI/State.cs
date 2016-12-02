f8caf81025d7176157d2210a49d71d32c865c22c
lazytaroice <jtshih@ucsc.edu>
2016-12-02 15:21:35 -0800

    added Kevin's stuff, hopefully all okay

--- Assets/Scripts/AI/State.cs
+++ Assets/Scripts/AI/State.cs
@@ -14,23 +14,6 @@ public class State{
 	// Use this for initialization
 	void Start () {
 	}
-		
-
-	public List<string> legal_moves () {
-		List<string> good_moves = new List<string> ();
-		good_moves.Add ("MoveUpLeft");
-		good_moves.Add ("MoveUpRight");
-		good_moves.Add ("MoveDownLeft");
-		good_moves.Add ("MoveDownRight");
-		good_moves.Add ("MoveUp");
-		good_moves.Add ("MoveDown");
-		good_moves.Add ("MoveLeft");
-		good_moves.Add ("MoveRight");
-		good_moves.Add ("DoNothing");
-		return good_moves;
-	}
-		
-
 
 	// Update is called once per frame
 	void Update () {
